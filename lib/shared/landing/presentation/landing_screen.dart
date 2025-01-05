302d3f2b5c8a0b4e7e2cc74f629dc21c44e9cc86
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2025-01-05 15:56:03 +0000

    add: include bottom nav bar

--- lib/shared/landing/presentation/landing_screen.dart
+++ lib/shared/landing/presentation/landing_screen.dart
@@ -1,4 +1,7 @@
+import 'package:ev_app/core/extension/context_extension.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class LandingScreen extends StatefulWidget {
@@ -17,6 +20,50 @@ class _LandingScreenState extends State<LandingScreen> {
           child: const ModelViewer(
             src: "assets/models/sedan-car.glb",
           ),
+        ),
+      ),
+      bottomNavigationBar: GlassmorphicContainer(
+        width: context.mediaQuery.size.width,
+        height: 60,
+        blur: 20,
+        alignment: Alignment.center,
+        linearGradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              const Color(0xFF000000).withOpacity(0.1),
+              const Color(0xFF000000).withOpacity(0.05),
+            ],
+            stops: const [
+              0.1,
+              1,
+            ]),
+        borderGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            const Color(0xFF000000).withOpacity(0.5),
+            const Color((0xFF000000)).withOpacity(0.5),
+          ],
+        ),
+        borderRadius: 2,
+        border: 1,
+        child: const Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              CupertinoIcons.car_detailed,
+            ),
+            Icon(
+              Icons.flash_on,
+            ),
+            Icon(
+              CupertinoIcons.map_fill,
+              color: Colors.black,
+            )
+          ],
         ),
       ),
     );
